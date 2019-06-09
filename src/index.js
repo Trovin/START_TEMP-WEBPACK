c5d25d6afb07407ebc0c1f3beda52a350fd81514
Trovin <ruslan_trovin@mail.ru>
2019-06-09 18:20:03 +0300

    add image and font loader(check this)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-import style from "./main.scss";
+//import styles
+import styles from "./assets/styles/main.scss";
+
+//import js
+import scripts from "./assets/custom-js/log";
+
+//import img
+import img from "./assets/images/logo/webpack-logo.png";
 
 console.log(`I'm a silly entry point`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,28 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebPackPlugin = require("html-webpack-plugin"); //html loader
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); //css loader
+const autoprefixer = require('autoprefixer'); //autoprefixer for styles
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');  //clear dist loader
+
+const imagemin = require('imagemin');
+const imageminGifsicle = require("imagemin-gifsicle");
+const imageminJpegtran = require("imagemin-jpegtran");
+const imageminOptipng = require("imagemin-optipng");
+const imageminSvgo = require("imagemin-svgo");
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+
+path = {
+    dist: {
+        scripts: 'assets/js/',
+        styles: 'assets/styles/',
+        images: 'assets/images/',
+        fonts: 'assets/fonts/'
+    }
+};
 
 module.exports = {
+    output: {
+        filename: path.dist.scripts + 'app.min.js'
+    },
     module: {
         rules: [
             {
@@ -30,9 +50,49 @@ module.exports = {
                 use: [
                     'style-loader',
                     MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    "sass-loader"
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: [
+                                autoprefixer({
+                                    "overrideBrowserslist": [
+                                        "> 1%",
+                                        "last 30 versions"
+                                    ]
+                                })
+                            ]
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
                 ]
+            },
+            {
+                test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[path][name].[ext]',
+                        context: 'src'
+                    }
+                }
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[path][name].[ext]',
+                        context: 'src'
+                    }
+                }
             }
         ]
     },
@@ -43,8 +103,30 @@ module.exports = {
             filename: "./index.html"
         }),
         new MiniCssExtractPlugin({
-            filename: "[name].css",
+            filename: path.dist.styles + "[name].css",
             chunkFilename: "[id].css"
+        }),
+        new ImageminPlugin({
+            bail: false, // Ignore errors on corrupted images
+            cache: true,
+            imageminOptions: {
+                // Lossless optimization with custom option
+                // Feel free to experement with options for better result for you
+                plugins: [
+                    imageminGifsicle({
+                        interlaced: true
+                    }),
+                    imageminJpegtran({
+                        progressive: true
+                    }),
+                    imageminOptipng({
+                        optimizationLevel: 5
+                    }),
+                    imageminSvgo({
+                        removeViewBox: true
+                    })
+                ]
+            }
         })
     ]
 };
