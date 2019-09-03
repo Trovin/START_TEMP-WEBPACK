4c3fd9dc5101944b9a223e95684e90b0ae6b5304
Trovin <ruslan_trovin@mail.ru>
2019-09-03 22:35:21 +0300

    change assembly to new simple starter temp

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-//import styles
-import styles from "./assets/styles/main.scss";
+import style from './assets/styles/main.scss';
 
-//import js
-import scripts from "./assets/custom-js/log";
+//images
+//import images from './assets/images/webpack.png';
 
-//import img
-import img from "./assets/images/logo/webpack-logo.png";
-
-console.log(`I'm a silly entry point`);
+console.log('init app');
--- webpack.config.js
+++ webpack.config.js
@@ -1,132 +1,71 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin"); //html loader
-const MiniCssExtractPlugin = require("mini-css-extract-plugin"); //css loader
-const autoprefixer = require('autoprefixer'); //autoprefixer for styles
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');  //clear dist loader
-
-const imagemin = require('imagemin');
-const imageminGifsicle = require("imagemin-gifsicle");
-const imageminJpegtran = require("imagemin-jpegtran");
-const imageminOptipng = require("imagemin-optipng");
-const imageminSvgo = require("imagemin-svgo");
-const ImageminPlugin = require('imagemin-webpack-plugin').default;
-
-path = {
-    dist: {
-        scripts: 'assets/js/',
-        styles: 'assets/styles/',
-        images: 'assets/images/',
-        fonts: 'assets/fonts/'
-    }
-};
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    output: {
-        filename: path.dist.scripts + 'app.min.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: "html-loader",
-                        options: { minimize: true }
-                    }
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader"]
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader'
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: [
-                                autoprefixer({
-                                    "overrideBrowserslist": [
-                                        "> 1%",
-                                        "last 30 versions"
-                                    ]
-                                })
-                            ]
-                        }
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    }
-                ]
-            },
-            {
-                test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-                use: {
-                    loader: 'file-loader',
-                    options: {
-                        name: '[path][name].[ext]',
-                        context: 'src'
-                    }
-                }
-            },
-            {
-                test: /\.(jpe?g|png|gif|svg)$/i,
-                use: {
-                    loader: 'file-loader',
-                    options: {
-                        name: '[path][name].[ext]',
-                        context: 'src'
-                    }
-                }
-            }
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
         ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebPackPlugin({
-            template: "./src/index.html",
-            filename: "./index.html"
-        }),
-        new MiniCssExtractPlugin({
-            filename: path.dist.styles + "[name].css",
-            chunkFilename: "[id].css"
-        }),
-        new ImageminPlugin({
-            bail: false, // Ignore errors on corrupted images
-            cache: true,
-            imageminOptions: {
-                // Lossless optimization with custom option
-                // Feel free to experement with options for better result for you
-                plugins: [
-                    imageminGifsicle({
-                        interlaced: true
-                    }),
-                    imageminJpegtran({
-                        progressive: true
-                    }),
-                    imageminOptipng({
-                        optimizationLevel: 5
-                    }),
-                    imageminSvgo({
-                        removeViewBox: true
-                    })
-                ]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              useRelativePath: true,
+              outputPath: 'assets/images',
+              publicPath: ''
+            },
+          },
+        ],
+      },
+      {
+         test: /\.(woff|woff2|eot|ttf|otf)$/,
+         use: [
+           'file-loader',
+         ]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+                publicPath: '../' // path to director where assets folder is located
             }
-        })
+          },
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader'
+        ]
+      }
     ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "styles/main.css"
+    })
+  ]
 };
